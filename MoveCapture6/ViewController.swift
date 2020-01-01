2238ebbd4bd25efe2fa0ba7107b1aa9454ed7d8e
Tatsuya Fujisaki <taco@xf6.so-net.ne.jp>
2020-01-02 06:30:53 +0900

    Initial Commit

---
+++ MoveCapture6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoveCapture6
+//
+//  Created by 藤崎達也 on 2020/01/02.
+//  Copyright © 2020 藤崎達也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
